0229beada8a6632960b7418062c2f98c0d8d59c9
unknown <putrayanto528@gmail.com>
2023-05-09 14:43:15 +0700

    version 2

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ArticleController;
+use App\http\Controllers\Api\AuthController;
 use App\Models\Article;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -15,11 +16,16 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
-Route::get('articles', [ArticleController::class, 'index']);
+Route::get('articles', [ArticleController::class, 'index'])->middleware('auth:sanctum');
 Route::get('articles/{article}', [ArticleController::class, 'show']);
 Route::post('articles', [ArticleController::class, 'store']);
 Route::put('articles/{article}', [ArticleController::class, 'update']);
 Route::delete('articles/{article}', [ArticleController::class, 'delete']);
+
+Route::post('/auth/register', [AuthController::class, 'createUser']);
+Route::post('/auth/login', [AuthController::class, 'loginUser']);
+
+
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
